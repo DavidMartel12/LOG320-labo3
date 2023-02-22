1bd1d381f6e7036acf7138a1ba1f8e192addce30
DavidMartel12 <marteldavid02@gmail.com>
2023-02-21 21:36:15 -0500

    Petite modif pour ne pas modifier la classe d'origine...

--- src/laboratoire3/DocDist.java
+++ src/laboratoire3/DocDist.java
@@ -2,21 +2,25 @@ package laboratoire3;
 
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 
 public class DocDist{
 
     // Ne pas changer cette fonction, elle sera utilisée pour tester votre programme
-    public double docDistance(String nomFichier1, String nomFichier2) throws IOException {
+    public double docDistance(String nomFichier1, String nomFichier2)
+    {
+        try{
+            Helper helper = new Helper();
+            HashMap<String, Integer> tableFichier1 = helper.createFrequencyTable(new File(nomFichier1));
+            HashMap<String, Integer> tableFichier2 = helper.createFrequencyTable(new File(nomFichier2));
+        }catch (IOException e){
+            System.err.println("ERROR: " + e);
+            throw new RuntimeException(e);
+        }
 
-        Helper helper = new Helper();
-
-        HashMap<String, Integer> tableFichier1 = helper.createFrequencyTable(new File(nomFichier1));
-        HashMap<String, Integer> tableFichier2 = helper.createFrequencyTable(new File(nomFichier2));
-
-        return 0.0; //Équation
+        return 0.0;
     }
+
 
 }
